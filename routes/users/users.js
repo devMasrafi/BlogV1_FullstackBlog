79bcd08cb67b3cf66383b5f5beb072b23b43b808
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-08-27 12:49:19 +0600

    Add files via upload

--- routes/users/users.js
+++ routes/users/users.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const multer = require('multer')
 const { registerCtrl, loginCtrl, userDetailsCtrl, profileDetailsCtrl, profilePhotoCtrl, coverPhotoCtrl, updatePassCtrl, userUpdateCtrl, logoutCtrl } = require('../../controllers/users/users')
-
 const protected = require('../../middlewares/protected')
+const storage = require('../../config/cloudinary')
+
 
 const userRoutes = express.Router()
+// instance of multer
+const upload = multer({storage})
+
 
 
 // register
@@ -20,10 +25,10 @@ userRoutes.get ("/profile/",protected, profileDetailsCtrl)
 userRoutes.get ("/:id", userDetailsCtrl)
 
 // put/api/v1/users/profile-upload/:id (profile image Upload)
-userRoutes.put ("/profile-photo-upload/:id", profilePhotoCtrl)
+userRoutes.put ("/profile-photo-upload/",upload.single("profile"),protected , profilePhotoCtrl)
 
 // put/api/v1/users/cover-photo-upload/:id (cover image upload)
-userRoutes.put ("/cover-photo-upload/:id", coverPhotoCtrl)
+userRoutes.put ("/cover-photo-upload/",upload.single("coverPhoto"), protected ,coverPhotoCtrl)
 
 // put/api/v1/users/update-password/:id (User password update)
 userRoutes.put ("/update-password/:id", updatePassCtrl)
